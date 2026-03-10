ce54fc115f19954b784677c488d8dcaf847f2163
illy <fluoriteByte@disroot.org>
2026-03-10 20:08:57 +0000

    + buildpak script ~ rest of changes needed for it to build correctly

---
+++ buildpak.fish
@@ -0,0 +1,28 @@
+#!/bin/env fish
+# just to script to help me not have to ctrl + r the command lol
+echo this flatpak is for building and packaging the launcher
+set APPID xyz.fnordmc.FnordLauncher
+set MANIFEST xyz.fnordmc.FnordLauncher.yml
+set _flatpak_opts --install-deps-from=flathub --force-clean .build --repo=.repo --ccache $MANIFEST
+
+argparse -x 'user,clean' 'u/user' 'c/clean' -- $argv
+
+if test -n "$_flag_c"
+  rm .repo
+  exit
+end
+
+if test -n "$_flag_u"
+  set -a _flatpak_opts --user
+end
+
+if command -v flatpak-builder
+  set FPBUILD (command -v flatpak-builder)
+else
+  set FPBUILD flatpak run org.flatpak.Builder
+end
+
+$FPBUILD $_flatpak_opts || exit
+set filename_to_use \
+  (basename -s .tar.gz (grep -o 'FnordLauncher-[^\"]*.tar.gz' $MANIFEST)).flatpak
+flatpak build-bundle .repo $filename_to_use $APPID
